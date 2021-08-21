2b0ddb5c8b9284c330b0c4ca48cf2bf9d9d1730d
Pascal Lauener <pascal@lauener.biz>
2021-08-21 10:49:25 +0200

    update

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 5.0
 dotnet --version
-dotnet publish -c Release
+dotnet publish -c Release -o output
