15df6cd01a86cc6155e9c58b60dba6030ff00035
Pascal Lauener <pascal@lauener.biz>
2021-08-21 10:55:05 +0200

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+echo "# Install dotnet sdk"
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 5.0
-dotnet --version
-dotnet publish -c Release -o output
+./dotnet-install.sh -c 5.0 -InstallDir ./dotnet5
+#./dotnet-install.sh -c 5.0
+
+echo "#ls"
+#ls -al
+
+echo "#show installed version"
+./dotnet5/dotnet --version
+#dotnet --version
+
+echo "#build"
+./dotnet5/dotnet publish -c Release -o output
+#dotnet publish -c Release -o output
