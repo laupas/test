dd994e91e41a434fd211d869805983809cfa8d92
Pascal Lauener <pascal.lauener@livinguard.com>
2021-08-30 09:56:11 +0200

    Add CMS

---
+++ Code/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace Lauener.Test {
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Radzen;
+using prismic;
+using prismic.fragments;
+
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 
 namespace blazor
 {
@@ -24,7 +29,43 @@ namespace blazor
             builder.Services.AddScoped<NotificationService>();
             builder.Services.AddScoped<TooltipService>();
             builder.Services.AddScoped<ContextMenuService>();
-            await builder.Build().RunAsync();
+            builder.Services.AddSingleton<IMemoryCache, MemoryCache>();
+
+            //prismic
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSingleton<ICache, InMemoryCache>();
+            builder.Services.AddHttpClient<PrismicHttpClient>();
+            builder.Services.AddSingleton<IPrismicApiAccessor, DefaultPrismicApiAccessor>();
+            builder.Services.AddSingleton<DocumentLinkResolver, DefaultDocumentLinkResolver>();
+            var option = new PrismicOption(builder.Configuration["token"]);
+            builder.Services.AddSingleton<IOptions<PrismicSettings>>(option);
+
+            var host = builder.Build();
+
+            await host.RunAsync();
+        }
+    }
+
+    public class PrismicOption : IOptions<PrismicSettings>
+    {
+        private string token;
+
+        public PrismicOption(string token)
+        {
+            this.token = token;
+        }
+        public PrismicSettings Value => new PrismicSettings(){
+            AccessToken = this.token,
+            Endpoint = "https://lauener.prismic.io/api/v1"
+        };
+    }
+
+    public class DefaultDocumentLinkResolver : DocumentLinkResolver
+    {
+        public override string Resolve(DocumentLink link)
+        {
+            return link.Uid.ToString();
         }
     }
 }
